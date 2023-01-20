2b6e7c6e0275d8a53fddbe96cbcac43ea4faa5ee
rettichsimonlel <retter190085@sr.htlweiz.at>
2023-01-20 12:22:28 +0100

    yes

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ import sqlalchemy.orm
 from model import Base, User, Data, Kalender
 
 calender = Calender()
-week = calender.make_week()
-calender.draw_week(week)
 
 def main():
     """Run Main function."""
     db_connection = database_connections("sqlite:///Calender.db")
     session_factory = session_creation(db_connection)
 
+    while (True):
+        calender.draw_week()
+
+        print()
+        m_input = input("(q)uit, (a)dd, (n)ext, (b)revios: ")
+        if m_input == "q":
+            break
+        
 
 def database_connections(database_url):
     """Connect to database."""
--- my_calender.py
+++ my_calender.py
@@ -5,20 +5,31 @@ class Calender():
     def __init__(self):
         self.current_week = datetime.datetime.now().isocalendar().week
         self.current_date = datetime.datetime.now()
+        self.monday = self.current_date + datetime.timedelta(days = -self.current_date.weekday())
+        self.current_monday = self.monday
 
     def make_week(self):
         week = []
-        monday = self.current_date + datetime.timedelta(days = -self.current_date.weekday())
-        week.append(monday)
+        week.append(self.current_monday)
         for i in range(1, 7):
-            next_day = monday + datetime.timedelta(days=i)
+            next_day = self.current_monday + datetime.timedelta(days=i)
             week.append(next_day)
         return week
 
-    def draw_week(self, week):
+    def draw_week(self):
+        week = self.make_week()
         for i in range(7):
             if week[i].day == datetime.datetime.now().day:
                 print(f"{bcolors.HEADER}{week[i].day}\t{bcolors.ENDC}", end="")
-            print(f"{week[i].day}\t", end="")
+            else:
+                print(f"{week[i].day}\t", end="")
         print()
+
+    def next_week(self):
+        self.current_monday = self.current_monday + datetime.timedelta(days=7)
+        self.draw_week()
+
+    def previos_week(self):
+        self.current_monday = self.current_monday - datetime.timedelta(days=7)
+        self.draw_week()
 
